4eb26de473518f63b41ad141ed8c059e89ab8dea
Andreina Francisco <maria.andreina.francisco@gmail.com>
2021-12-21 11:01:12 +0100

    Update regression-tests.sh

--- regression-tests.sh
+++ regression-tests.sh
@@ -5,41 +5,41 @@ echo "Hello, world!"
 myUnitTests=( 'pl-example01' '4 plates' '16'
 	      'pl-example02' 'MiniZinc: evaluation error:' '1'
 	      'pl-example03' 'MiniZinc: evaluation error:' '1'
-	      'pl-example04-jonne-doubled' '2 plates' '319'
 	      'pl-example05' '2 plates' '1'
 	      'pl-example06' '2 plates' '1'
 	      'pl-example07-tiny' '4 plates' '1'
 	      'pl-example08-small' '2 plates' '3'
 	      'pl-example09' '2 plates' '2'
 	      'pl-example10' '4 plates' '14'
-	      'pl-example11' '2 plates' ''
-	      'pl-example12' '2 plates' ''
-	      'pl-example13' '1 plates' ''
-	      'pl-example14' '1 plates' ''
-	      'pl-example15' '1 plates' ''
-	      'pl-example17' '1 plates' ''
-	      'pl-example18' '1 plates' ''
-	      'pl-example19' '1 plates' ''
-	      'pl-example21' '1 plates' ''
-	      'pl-example22' '4 plates' ''
+	      'pl-example11' '2 plates' '230'
+	      'pl-example12' '2 plates' '1'
+	      'pl-example13' '1 plates' '5'
+	      'pl-example14' '1 plates' '1'
+	      'pl-example15' '1 plates' '1'
+	      'pl-example17' '1 plates' '1'
+	      'pl-example18' '1 plates' '1'
+	      'pl-example19' '1 plates' '3'
+	      'pl-example21' '1 plates' '1'
+	      'pl-example22' '4 plates' '1'
 	      #'pl-example23' '2 plates' '' #Duplicated example
-	      'pl-example24' '2 plates' ''
-	      'pl-example25' '2 plates' ''
-	      'pl-example27' '3 plates' ''
-	      'pl-example28' '4 plates' ''
-	      'pl-example29' '2 plates' ''
-	      'pl-example30' '1 plates' ''
-	      'pl-example31' 'MiniZinc: evaluation error:' ''
-	      'pl-example35' '1 plates' ''
-	      'pl-example36' 'MiniZinc: evaluation error:' ''
-	      'pl-example37' '1 plates' ''
-	      '2020-10-08-jonne-slack' '1 plates' ''
-	      '2020-11-13-jonne-slack' '1 plates' ''
+	      'pl-example24' '2 plates' '424'
+	      'pl-example25' '2 plates' '12'
+	      'pl-example27' '3 plates' '2'
+	      'pl-example28' '4 plates' '4'
+	      'pl-example29' '2 plates' '1'
+	      'pl-example30' '1 plates' '82'
+	      'pl-example31' 'MiniZinc: evaluation error:' '1'
+	      'pl-example35' '1 plates' '66'
+	      'pl-example36' 'MiniZinc: evaluation error:' '1'
+	      'pl-example37' '1 plates' '1'
+	      '2020-10-08-jonne-slack' '1 plates' '21'
+	      '2020-11-13-jonne-slack' '1 plates' '3'
 	      #'compounds-10-9-3' #'1 plates' '' #'compounds-10-9-3'
-	      'dose-response-20-3-1' '1 plates' ''
-	      'dose-response-20-3-2' '1 plates' ''
-	      'dose-response-20-3-3' '1 plates' ''
-	      'pl-example42' '1 plates' ''
+	      'dose-response-20-3-1' '1 plates' '22'
+	      'dose-response-20-3-2' '1 plates' '21'
+	      'dose-response-20-3-3' '1 plates' '36'
+	      'pl-example04-jonne-doubled' '2 plates' '319'
+	      'pl-example42' '1 plates' '336'
 	      'pl-example20' '1 plates' ''
 	      #'2020-09-30-jonne-slack' #'1 plates'
 	    )
@@ -53,9 +53,13 @@ do
     echo "Usually takes about ${myUnitTests[${i}+2]} sec."
 
     SECONDS=0
-    
-    /Applications/MiniZincIDE.app/Contents/Resources/minizinc --solver Gecode plate-design.mzn ${myUnitTests[${i}]}.dzn --cmdline-data "testing=true"  &> ${myUnitTests[${i}]}.txt
-    
+
+    # Deterministic solution
+#    /Applications/MiniZincIDE.app/Contents/Resources/minizinc --solver Gecode plate-design.mzn ${myUnitTests[${i}]}.dzn --cmdline-data "testing=true"  &> ${myUnitTests[${i}]}.txt
+
+    # Random and multi-thread
+    /Applications/MiniZincIDE.app/Contents/Resources/minizinc --solver Gecode plate-design.mzn ${myUnitTests[${i}]}.dzn -p 10 -r $RANDOM --cmdline-data "testing=true"  &> ${myUnitTests[${i}]}.txt
+
     echo "It took about $SECONDS sec."
     
     read -r line < ${myUnitTests[${i}]}.txt
@@ -75,13 +79,14 @@ do
 
     # Compare that the solution is the same as before (so hopefully we haven't lost any solutions)
     
-    if cmp "${myUnitTests[${i}]}.txt" "./output-regression-tests/${myUnitTests[${i}]}.txt";
-    then
-	echo "The solution has not changed :-)\n"
-    else
-	echo "WARNING: Check ${myUnitTests[${i}]}.dzn! Something changed!!!!!!!!\n"
-    fi
+#    if cmp "${myUnitTests[${i}]}.txt" "./output-regression-tests/${myUnitTests[${i}]}.txt";
+ #   then
+#	echo "The solution has not changed :-)"
+ #   else
+#	echo "WARNING: Check ${myUnitTests[${i}]}.dzn! Something changed!!!!!!!!\n"
+ #   fi
 
+    echo "\n"
 done
 
 echo "We're done now! Bye!"
