81f65ec511da16e4d021f52968a5facad2aee809
Andreina Francisco <maria.andreina.francisco@gmail.com>
2025-01-29 19:22:22 +0100

    Proof of concept for a new variable model 🌻
    Started by splitting the main model into multiple files and cleaning some stuff that is old and not really useful anymore. I will have to finally update the input format but it's silly to have so much old stuff only for backwards compatibility at this point.

---
+++ proof-of-concept/regression-tests.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+echo "\nHello, world! Let's check that our PLAID constraint model v2 behaves as expected...\n"
+
+## FileName N.Plates Time
+myUnitTests=( 'pl-example-001' '1' '10'
+	    )
+
+len=${#myUnitTests[@]}
+
+for (( i=0; i<$len; i=i+3 ))
+do
+    echo "Testing file ${myUnitTests[${i}]}.dzn"
+    
+    echo "Expecting ${myUnitTests[${i+1}]} plates"
+    
+    echo "Usually takes about ${myUnitTests[${i}+2]} sec."
+
+    SECONDS=0
+
+    # Deterministic solution
+#    /Applications/MiniZincIDE.app/Contents/Resources/minizinc --solver Gecode plate-design.mzn ${myUnitTests[${i}]}.dzn --cmdline-data "testing=true"  &> ${myUnitTests[${i}]}.txt
+
+    # Random and multi-thread
+    /Applications/MiniZincIDE.app/Contents/Resources/minizinc --solver Gecode plate-design.mzn dzn-examples/${myUnitTests[${i}]}.dzn -p 10 -r $RANDOM --cmdline-data "testing=true"  &> regression-tests-results/${myUnitTests[${i}]}.txt
+
+    echo "It took about $SECONDS sec."
+    
+    read -r result_line < regression-tests-results/${myUnitTests[${i}]}.txt
+    
+    set -- ${myUnitTests[${i}+1]} 
+
+    
+    # Check that we have the expected number of plates or expected error
+    
+    if [[ $result_line == "${myUnitTests[${i+1}]} plates" || $1 == "assert" ]]
+    then
+	echo "We are happy with the results of ${myUnitTests[${i}]}.txt :-)"
+    else
+	echo "ERROR: The number of plates in ${myUnitTests[${i}]}.dzn changed!!!!!!!!"
+	break
+    fi   
+
+    echo "\n"
+done
+
+echo "We're done now! Bye!"
